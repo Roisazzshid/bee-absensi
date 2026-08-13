271ffe21c57cd61d86c57f0e308f75cff8fda8c4
Roisazzshid3 <isamiq3@gmail.com>
2026-08-13 16:01:56 +0700

    edit api

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://127.0.0.1:8000/api").replace(/\/$/, "");
+export const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://yeastlike-liefly-marleen.ngrok-free.dev/api").replace(/\/$/, "");
 
 export type ApiResponse<T> = { status: "success" | "error"; message: string; data: T; errors?: Record<string, string[] | string> | null };
 
