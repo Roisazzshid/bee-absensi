f57b6a7cb0c886ab5105f681f900677a98fda307
Roisazzshid3 <isamiq3@gmail.com>
2026-08-21 15:06:58 +0700

    update api

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://pub-outsourcing-willow-released.trycloudflare.com/api").replace(/\/$/, "");
+export const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://kprdy-125-165-188-185.run.pinggy-free.link/api").replace(/\/$/, "");
 
 export type ApiResponse<T> = { status: "success" | "error"; message: string; data: T; errors?: Record<string, string[] | string> | null };
 
